7c6033154a6e0987d94e5580954fb8b6e4793d8a
mostafaelgazar526 <mostafaelgazar526@gmail.com>
2026-09-17 14:35:07 +0300

    Update career section and image upload

--- src/app/layout/pages/career/career.component.ts
+++ src/app/layout/pages/career/career.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-career',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './career.component.html',
   styleUrl: './career.component.css'
 })
 export class CareerComponent {
 
+selectedImage: File | null = null;
+imagePreview: string | null = null;
+
+onFileSelected(event: Event): void {
+
+  console.log('File input changed');
+
+  const input = event.target as HTMLInputElement;
+
+  if (!input.files || input.files.length === 0) {
+    console.log('No file selected');
+    return;
+  }
+
+  this.selectedImage = input.files[0];
+
+  console.log('Selected file:', this.selectedImage);
+
+  const reader = new FileReader();
+
+  reader.onload = () => {
+
+    console.log('Image loaded');
+
+    this.imagePreview = reader.result as string;
+
+  };
+
+  reader.readAsDataURL(this.selectedImage);
+}
+
+  
 }
